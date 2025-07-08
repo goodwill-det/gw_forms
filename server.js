e3ad5f18caf948b911d088175c64bb32d29ec5d8
reemrizk <reemrizk244@gmail.com>
2025-07-08 15:55:08 -0400

    jnefrf

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require("dotenv").config({ path: envPath });
-console.log("DEBUG → DB_USER:", process.env.DB_USER);
-console.log("DEBUG → DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DEBUG → DB_HOST:", process.env.DB_HOST);
-
 const path = require("path");
 // load .env.production only when in prod
 const envPath = process.env.NODE_ENV === 'production'
   ? path.resolve(__dirname, ".env.production")
   : path.resolve(__dirname, ".env.development");
+
+require("dotenv").config({ path: envPath });
+console.log("DEBUG → DB_USER:", process.env.DB_USER);
+console.log("DEBUG → DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DEBUG → DB_HOST:", process.env.DB_HOST);
 
 
 const express = require("express");
