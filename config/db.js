277ba42617417a76739e5bc235b5b3074caf24fc
reemrizk <reemrizk244@gmail.com>
2025-07-08 14:21:17 -0400

    updated config, server, db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'user1',
-  password: 'G00dW!11',
-  database: 'evaluation_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
-const app = express();
-const PORT = 3000;
+
 
 const employeeRoutes = require("./routes/employees");
 const evaluationRoutes = require("./routes/evaluations");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || 'localhost';
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -19,6 +25,6 @@ app.use("/api/login", authRoutes);
 app.use("/api/evaluations", evaluationRoutes);
 
 // Start server
-app.listen(PORT, "goodwilldetroit.online", () => {
-  console.log(` Server running at http://goodwilldetroit.online:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(` Server running at http://${HOST}:${PORT}`);
 });
