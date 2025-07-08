7d77987f2d0459bc915f515db5fd8f257ab394cd
reemrizk <reemrizk244@gmail.com>
2025-07-08 15:49:52 -0400

    jnefrf

--- config/db.js
+++ config/db.js
@@ -2,6 +2,13 @@
 
 const mysql = require('mysql2');
 
+console.log(" USING DB CREDENTIALS:", {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME
+});
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
